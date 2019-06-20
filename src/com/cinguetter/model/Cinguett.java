12b0b64809dd0525d7949ce31c62739a5ee02a57
Alessandro <Alessandro@192.168.20.136>
2019-06-20 14:22:34 +0200

    aggiunto l'arrayList nella classe cinguett

--- src/com/cinguetter/model/Cinguett.java
+++ src/com/cinguetter/model/Cinguett.java
@@ -1,8 +1,10 @@
 package com.cinguetter.model;
 
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
 public class Cinguett extends PostedMessage {
+	private ArrayList<Comment> commentsList = new ArrayList<Comment>();
 
 	public Cinguett(String text, String userEmail, GregorianCalendar postedTime, int id) {
 		super(text, userEmail, postedTime, id);
