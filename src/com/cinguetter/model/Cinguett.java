c6ae649c4372b3284233192c520c75db421401d4
Alessandro <Alessandro@192.168.20.136>
2019-06-20 14:26:07 +0200

    aggiunti i get e i set dell'arrayList nella classe cinguett

--- src/com/cinguetter/model/Cinguett.java
+++ src/com/cinguetter/model/Cinguett.java
@@ -4,7 +4,15 @@ import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
 public class Cinguett extends PostedMessage {
-	private ArrayList<Comment> commentsList = new ArrayList<Comment>();
+	private ArrayList<Comment> commentsList;
+
+	public ArrayList<Comment> getComments() {
+		return commentsList;
+	}
+
+	public void setComments(ArrayList<Comment> commentsList) {
+		this.commentsList = commentsList;
+	}
 
 	public Cinguett(String text, String userEmail, GregorianCalendar postedTime, int id) {
 		super(text, userEmail, postedTime, id);
