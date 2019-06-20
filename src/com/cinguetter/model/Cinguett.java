b310e594d61d98fbe55a6fafc1743fdf84418c9e
Alessandro <Alessandro@192.168.20.136>
2019-06-20 14:28:12 +0200

    eliminate le modifiche

--- src/com/cinguetter/model/Cinguett.java
+++ src/com/cinguetter/model/Cinguett.java
@@ -1,18 +1,8 @@
 package com.cinguetter.model;
 
-import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
 public class Cinguett extends PostedMessage {
-	private ArrayList<Comment> commentsList;
-
-	public ArrayList<Comment> getComments() {
-		return commentsList;
-	}
-
-	public void setComments(ArrayList<Comment> commentsList) {
-		this.commentsList = commentsList;
-	}
 
 	public Cinguett(String text, String userEmail, GregorianCalendar postedTime, int id) {
 		super(text, userEmail, postedTime, id);
