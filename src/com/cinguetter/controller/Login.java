8637090b3ee337acc490c141483025b305764633
LV4 <LV4@192.168.20.174>
2019-06-21 12:40:07 +0200

    Servlet Login implementation

--- src/com/cinguetter/controller/Login.java
+++ src/com/cinguetter/controller/Login.java
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.cinguetter.model.UserFactory;
 
 /**
  * Servlet implementation class Login
@@ -13,29 +16,57 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/index.html")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			if ((boolean) session.getAttribute("logout")) {
+				session.invalidate();
+				request.getRequestDispatcher("WEB-INF/JSP/login.jsp").forward(request, response);
+			} else {
+				response.sendRedirect("home.html");
+			}
+		} else {
+			request.getRequestDispatcher("WEB-INF/JSP/login.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		if (email != null && password != null && UserFactory.getInstance().login(email, password) == true) {
+			HttpSession session = request.getSession();
+			session.setAttribute("email", email);
+			session.setAttribute("password", password);
+			response.sendRedirect("home.html");
+
+		} else {
+			request.setAttribute("Error", "Wrong credentials");
+			request.setAttribute("logged", false);
+			request.getRequestDispatcher("WEB-INF/JSP/login.jsp").forward(request, response);
+
+		}
+
 	}
 
 }
