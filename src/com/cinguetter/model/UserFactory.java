325bdf67436a3b49cec4589f102e29de96e85747
marco <marco@192.168.20.151>
2019-06-21 12:23:54 +0200

    Implented login method in UserFactory

--- src/com/cinguetter/model/UserFactory.java
+++ src/com/cinguetter/model/UserFactory.java
@@ -1,6 +1,12 @@
 package com.cinguetter.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.GregorianCalendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserFactory {
 
@@ -21,12 +27,30 @@ public class UserFactory {
 			GregorianCalendar birthday) {
 		return true;
 	}
-	
+
 	public boolean login(String email, String password) {
-		return true;
+		String sql = "select name from users where email= ? and password= ?";
+
+		try (Connection conn = DbManager.getInstance().getDbConnection();
+				PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, email);
+			stmt.setString(2, password);
+
+			ResultSet result = stmt.executeQuery();
+
+			if (result.next()) {
+				return true;
+			}
+
+		} catch (SQLException e) {
+			Logger.getLogger(UserFactory.class.getName()).log(Level.SEVERE,null,e);
+			System.out.println("errore in login dentro UserFactory");
+		}
+		return false;
 	}
-	
-	public boolean editDetails(String newName, String newSurname, String newEmail, String newUrlImageProfile, String newPassword, String email) {
+
+	public boolean editDetails(String newName, String newSurname, String newEmail, String newUrlImageProfile,
+			String newPassword, String email) {
 		return true;
 	}
 
