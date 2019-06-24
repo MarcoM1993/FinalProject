ea5f254fceb93e5c034b93fd498aa40a2005581a
Mara Medda <Mara Medda@192.168.20.152>
2019-06-24 12:15:49 +0200

    implemented getUser method in User Factory

--- src/com/cinguetter/model/UserFactory.java
+++ src/com/cinguetter/model/UserFactory.java
@@ -1,10 +1,12 @@
 package com.cinguetter.model;
 
+import java.nio.channels.SelectableChannel;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.GregorianCalendar;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -43,7 +45,7 @@ public class UserFactory {
 			}
 
 		} catch (SQLException e) {
-			Logger.getLogger(UserFactory.class.getName()).log(Level.SEVERE,null,e);
+			Logger.getLogger(UserFactory.class.getName()).log(Level.SEVERE, null, e);
 			System.out.println("errore in login dentro UserFactory");
 		}
 		return false;
@@ -53,9 +55,33 @@ public class UserFactory {
 			String newPassword, String email) {
 		return true;
 	}
-	
+
 	public User getUser(String email) {
-	// TO DO	
+		String sql = "select name, surname, email, urlimageprofile, birthday from users where email= ?";
+
+		try (Connection conn = DbManager.getInstance().getDbConnection();
+				PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, email);
+			ResultSet result = stmt.executeQuery();
+
+			while (result.next()) {
+				String name = result.getString("name");
+				String surname = result.getString("surname");
+				String urlImageProfile = result.getString("urlImageProfile");
+				// GregorianCalendar birthday=result.getGregorianCalendar("birthday");
+
+				User user = new User(name, surname, email, urlImageProfile, new GregorianCalendar());// TODO gestire le
+																										// logiche date
+				return user;
+			}
+
+		} catch (SQLException e) {
+			Logger.getLogger(UserFactory.class.getName()).log(Level.SEVERE, null, e);
+			System.out.println("errore in getUser dentro UtenteFactory");
+
+		}
+		return null;
+
 	}
 
 }
