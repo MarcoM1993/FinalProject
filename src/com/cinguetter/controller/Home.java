c41fa49ed25fdfe94edcbe683c0649d85f3c6da1
Federico Spiga <f.spiga13@gmail.com>
2019-06-25 16:21:33 +0200

    Home servlet

--- src/com/cinguetter/controller/Home.java
+++ src/com/cinguetter/controller/Home.java
@@ -1,38 +1,54 @@
 package com.cinguetter.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import com.cinguetter.model.Cinguett;
 import com.cinguetter.model.CinguettFactory;
+import com.cinguetter.model.PostedMessage;
 import com.cinguetter.model.UserFactory;
-
 
 @WebServlet("/home.html")
 public class Home extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-  
-    public Home() {
-        super();
-      
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("cinguettList",CinguettFactory.getInstance().getCinguetts(10));
-		request.setAttribute("userMap",UserFactory.getInstance().getUsersMap(CinguettFactory.getInstance().getCinguetts(10)));
-		request.getRequestDispatcher("WEB-INF/JSP/home.jsp").forward(request, response);
+	public Home() {
+		super();
+
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = (String) session.getAttribute("email");
+		String password = (String) session.getAttribute("password");
 
-		doGet(request, response);
+
+		if (email != null && password != null && !email.isEmpty() && !password.isEmpty()
+				&& UserFactory.getInstance().login(email, password) == true) {
+			
+			List<PostedMessage> cinguetts = CinguettFactory.getInstance().getCinguetts(10);
+
+			request.setAttribute("cinguettList", cinguetts);
+			request.setAttribute("userMap", UserFactory.getInstance().getUsersMap(cinguetts));
+			request.getRequestDispatcher("WEB-INF/JSP/home.jsp").forward(request, response);
+
+		} else {
+			response.sendRedirect("index.html");
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 	}
 
 }
